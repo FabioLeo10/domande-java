bf5bf9702258e1c9c7b769ea2732ade8c9ad8382
FabioLeo10 <168586638+FabioLeo10@users.noreply.github.com>
2024-05-16 16:29:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//cos'è un algoritmo?
+//Un algoritmo è un insieme di istruzioni scritte per risolvere un problema
+
+//cos'è una variabile?
+//le variabili sono contenitori per dati parzialmente elaborati e vengono dichiarate con "let" e "const"
+
+//undefined e null sono la stessa cosa?
+//no, undefined si utilizza per una variabile di cui non si conosce il valore ma a cui sara`poi attribuito, null non avra`nessun tipo di valore
